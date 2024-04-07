862233fde6cd35870e74ebfe73129bec7b6b905f
hellcat <broum226.i@gmail.com>
2024-04-07 14:27:42 +0900

    240407_1

---
+++ src/components/custom-section.vue
@@ -0,0 +1,11 @@
+<template>
+  <UIcon :name="iconName" />
+  <slot name="header" />
+  <UDivider />
+  <div>
+    <slot />
+  </div>
+</template>
+<script setup lang="ts">
+defineProps(['iconName'])
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,19 @@
 <template>
   <h1>HELLO NUXT</h1>
-  <UButton>Button</UButton>
-  <UIcon name="i-heroicons-light-bulb"></UIcon>
+  <UContainer>
+    <UCard>
+      <CustomSection iconName="i-heroicons-user-circle">
+        <template #header>Profile</template>
+        ddd
+      </CustomSection>
+      <CustomSection iconName="i-heroicons-cube">
+        <template #header>Skills</template>
+        ddd
+      </CustomSection>
+      <CustomSection iconName="i-heroicons-identification">
+        <template #header>Contacts</template>
+        ddd
+      </CustomSection>
+    </UCard>
+  </UContainer>
 </template>
